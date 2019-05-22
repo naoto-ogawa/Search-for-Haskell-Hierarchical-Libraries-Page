6bae983fe9de97b4a1abd3149b6513e9c56162f1
Naoto Ogawa <becausethespiderspiedher+github@gmail.com>
2019-05-22 21:04:30 +0900

    add events for each library page.

---
+++ HHLE2.js
@@ -0,0 +1,26 @@
+// Haskell Hierarchical Libraries Easier
+
+const toggle = e => e.style.display = e.style.display === 'none' ? 'block' : 'none';
+
+const hideAll = function(sel) {
+  document.querySelectorAll(sel).forEach(e => toggle(e));
+}
+
+// hide all explanations.
+hideAll('.doc')
+
+var allsrc = document .querySelectorAll('.src')
+Array.prototype.slice.call(allsrc, 1) // excludes the description of the header.
+  .forEach(
+    e => e.addEventListener('click',
+      x => toggle(x.target.nextSibling)
+    )
+  );
+
+// event for a header.
+document.querySelector('#module-header').addEventListener('click',
+  e => toggle(document.querySelector('#description > .doc'))
+)
+
+// close instances.
+document.querySelectorAll('details').forEach(e => e.removeAttribute('open',''));
